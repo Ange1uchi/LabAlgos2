c37bad9820d8a03f7abb1d68bca695d35b6e7f20
Angel <angelvalerocelaya@gmail.com>
2024-05-20 09:56:02 -0400

    Cambiar la busqueda linea la busqueda binaria para evitar el TLE

--- Semana3/Spoj3.cpp
+++ Semana3/Spoj3.cpp
@@ -11,44 +11,41 @@ bool ok(vector<int>& v, int x, int c)
             d = v[i];
             count++;
         }
-        else {
-            continue;
+        if (count == c) {
+            return true;
         }
-    }
-    if (count >= c) {
-        return true;
     }
     return false;
 }
 
 int agresivacas(vector<int>& v, int n, int k)
 {
-    long long ans = -1;
-    int maxi = 0;
-    for (int i = 0; i < n; i++) {
-        maxi = max(maxi, v[i]);
-    }
-
-    for (long long i = 1; i <= maxi; i++) {
-        if (ok(v, i, k)) {
-            ans = i;
-        }
-        else {
-            break;
+    sort(v.begin(), v.end());
+    int low = 0, high = v[n-1] - v[0];
+    while (low < high) {
+        int mid = low + (high - low + 1) / 2;
+        if (ok(v, mid, k)) {
+            low = mid;
+        } else {
+            high = mid - 1;
         }
     }
- 
-    return ans;
+    return low;
 }
 
 int main()
 {
-    int K = 3;
-    vector<int> arr = { 1, 2, 4, 8, 9 };
-    int N = arr.size();
- 
-    int ans = agresivacas(arr, N, K);
-    cout << ans << endl;
- 
+    int t;
+    cin >> t;
+    while(t--) {
+        int N, K;
+        cin >> N >> K;
+        vector<int> arr(N);
+        for(int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        int ans = agresivacas(arr, N, K);
+        cout << ans << endl;
+    }
     return 0;
 }
